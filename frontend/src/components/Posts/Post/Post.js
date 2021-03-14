fae6a4d986958eee09920f454feb5ffd51a54205
ngocdu <ngocdu171@gmail.com>
2021-03-14 08:34:33 +0200

    get data and show them to post.js

--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -1,13 +1,56 @@
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@material-ui/core';
+import { Delete, MoreHoriz, ThumbUpAlt } from '@material-ui/icons';
+import moment from 'moment';
 import React from 'react';
 import useStyles from './styles';
 
-const Post = () => {
+const Post = ({post}) => {
     const classes = useStyles();
 
     return (
-        <h1>
-            POST
-        </h1>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} 
+                image={Post.selectedFile}
+                title={Post.title}
+            />
+            <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{color: 'white'}} size="small" 
+                    onClick={() => {}}
+                ><MoreHoriz fontSize="default" />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">
+                    {post.tags.map((tag)=>`#${tag}`)}
+                </Typography>
+            </div>
+            <CardContent>
+                <Typography variant="h5" gutterBottom
+                    className={classes.title}
+                >
+                    {post.message}
+                </Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary"
+                    onClick={()=>{}}
+                >
+                    <ThumbUpAlt fontSize="small" />
+                    Like
+                    {post.likeCount}
+                </Button>
+                <Button size="small" color="primary"
+                    onClick={()=>{}}
+                >
+                    <Delete fontSize="small" />
+                    Delete
+                </Button>
+            </CardActions>
+        </Card>
     )
 }
 
--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,3 +1,4 @@
+import { CircularProgress, Grid } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
 
@@ -11,12 +12,16 @@ const Posts = () => {
     console.log(posts);
     
     return (
-        <React.Fragment>
-            <h1>POSTS</h1>
-            <Post />
-            <Post />
-        </React.Fragment>
-    )
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
+    );
 }
 
 export default Posts;
