4770fc93530b9be129d9c7da920aa200bdd2932b
ngocdu <ngocdu171@gmail.com>
2021-03-17 11:40:15 +0200

    environmental variables

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 import postRoute from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,10 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoute);
 
-const CONNECTION_URL = 'mongodb+srv://BrotherD:conmemay7@cluster0.jabiy.mongodb.net/<dbname>?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://BrotherD:conmemay7@cluster0.jabiy.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((err) => console.log(err));
 
