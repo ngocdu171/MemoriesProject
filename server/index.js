cf81719762810763d4017f54982d488fcc57aab4
ngocdu <ngocdu171@gmail.com>
2021-03-18 09:01:41 +0200

    add new functions

--- server/index.js
+++ server/index.js
@@ -2,12 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
 import postRoute from './routes/posts.js';
 
 const app = express();
-dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -19,10 +17,11 @@ app.get('/', (req, res) => {
     res.send("Welcome to my World!!!");
 })
 
-// const CONNECTION_URL = 'mongodb+srv://BrotherD:conmemay7@cluster0.jabiy.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://BrotherD:conmemay7@cluster0.jabiy.mongodb.net/<dbname>?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((err) => console.log(err));
 
